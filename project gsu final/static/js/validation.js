d706a7ab724d07ad7a84e1fd724d14a06f9ac319
Spade <47337405+SagarPateI@users.noreply.github.com>
2023-04-19 23:41:06 -0400

    Moved the form validation under the login and added a sign in attempt cooldown

--- project gsu final/static/js/validation.js
+++ project gsu final/static/js/validation.js
@@ -2,24 +2,39 @@ const username = document.getElementById('username')
 const password = document.getElementById('password')
 const form = document.getElementById('form')
 const errorElement = document.getElementById('error')
+
+let loginAttempts = 0
+const maxLoginAttempts = 5
+const loginAttemptPeriodInMinutes = 10
+const loginAttemptsResetTime = loginAttemptPeriodInMinutes * 60 * 1000
+const lastLoginAttemptTime = new Date()
+
 form.addEventListener('submit', (e) => {
     let messages = []
     const commonPasswords = ['password', '123456', 'qwerty', 'letmein', 'monkey']
-    if (username.value === '' || username.value == null) {
-        messages.push('Username is required')
+    const timeSinceLastLoginAttempt = new Date() - lastLoginAttemptTime
+    if (loginAttempts >= maxLoginAttempts && timeSinceLastLoginAttempt < loginAttemptsResetTime) {
+        messages.push(`Too many login attempts. Please try again in ${loginAttemptsResetTime / 1000 / 60} minutes.`)
+    } else {
+        if (username.value === '' || username.value == null) {
+            messages.push('Username is required')
+        }
+        if (username.value.length < 5) {
+            messages.push('Username must be at least 5 characters long')
+        }
+        if (password.value.length < 8) {
+            messages.push('Password must be at least 8 characters long')
+        }
+        if (commonPasswords.includes(password.value.toLowerCase())) {
+            messages.push('Password is too common. Please choose a stronger password.')
+        }
+        if (messages.length === 0) {
+            loginAttempts = 0 // Reset login attempts
+            lastLoginAttemptTime = new Date()
+        } else {
+            loginAttempts += 1
+            e.preventDefault()
+        }
     }
-    if (username.value.length < 5) {
-        messages.push('Username must be at least 5 characters long')
-    }
-    if (password.value.length < 8) {
-        messages.push('Password must be at least 8 characters long')
-    }
-    if (commonPasswords.includes(password.value.toLowerCase())) {
-        messages.push('Password is too common. Please choose a stronger password.')
-    }
-    if (messages.length > 0) {
-        e.preventDefault()
-        errorElement.innerText = messages.join(', ')
-    }
+    errorElement.innerText = messages.join(', ')
 })
-
