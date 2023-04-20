0e0e493982cc93b048d7cf9fb4fd747db107d7c0
Spade <47337405+SagarPateI@users.noreply.github.com>
2023-04-19 22:13:37 -0400

    Bugfixing the form validation

--- project gsu final/static/js/validation.js
+++ project gsu final/static/js/validation.js
@@ -1,11 +1,11 @@
-const name = document.getElementById('name')
+const username = document.getElementById('username')
 const password = document.getElementById('password')
 const form = document.getElementById('form')
 const errorElement = document.getElementById('error')
 form.addEventListener('submit', (e) => {
     let messages = []
-    if (name.value === '' || name.value == null) {
-        messages.push('Name is required')
+    if (username.value === '' || username.value == null) {
+        messages.push('Username is required')
     }
     if (messages.length > 0) {
         e.preventDefault()
